038ca55342603acac1359a79db0c264e7ca1f7c0
Dustin Morando <dustinmorando@me.com>
2015-03-10 21:27:22 -0400

    first attempt at chartest

--- app/routes/opponents.js
+++ app/routes/opponents.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function () {
+    return {
+      chartData: {
+        labels: ['Day1', 'Day2', 'Day3'],
+        series: [
+          [5, 4, 8],
+          [10, 2, 7],
+          [8, 3, 6]
+        ]
+      }
+    };
+  }
 });
