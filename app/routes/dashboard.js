0ac38e7497244c6f03bcdac8a750878a1e493ca2
Dustin Morando <dustinmorando@me.com>
2015-03-12 17:18:34 -0400

    made fake object

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -3,13 +3,24 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function () {
     return {
-      chartData: {
+      locationData: {
         labels: ['Player1', 'Player2', 'Player3'],
         series: [
           [8, 6, 4],
-
         ]
-      }
+      },
+      opponentsData: {
+        labels: ['Player1', 'Player2', 'Player3', 'Player4'],
+        series: [
+          [8, 6, 4, 5],
+        ]
+      },
+      bankrollData: {
+        labels: ['Day1', 'Day2', 'Day3','Day4','Day5','Day5','Day6','Day6','Day7','Day8','Day9','Day10'],
+        series: [
+          [8, 6, 4, 7, 5, 2, 10, 9, 6, 9,],
+        ]
+      },
     };
   }
 });
