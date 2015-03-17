ce01eea91dacf7ece6aaa2c3f36ae296df3bec90
Dustin Morando <dustinmorando@me.com>
2015-03-17 15:24:13 -0400

    api working

--- app/routes/hands.js
+++ app/routes/hands.js
@@ -1,10 +1,24 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model: function(){
-    return Ember.$.ajax("https://random-hands.herokuapp.com/hands/1").then(function(response) {
-      console.log(response);
-      return response.results;
-    });
-  }
+ model: function() {
+   return Ember.$.ajax('https://random-hands.herokuapp.com/hands/1').then(function(data) {
+     console.log(data);
+     console.log(data.slice(-1).pop());
+     return data.slice(-1).pop();
+   }).then(function(hands) {
+     console.log(hands.slice(-1).pop());
+     return hands.slice(-1).pop();
+   }).then(function(hand){
+     if (hand.seatsInfo) {
+       console.log('Seat Info Found');
+       var tableStack = hand.seatsInfo.reduce(function(acc, player) {
+         return acc + Number(player.stackSize);
+       }, 0);
+       console.log('tableStack: ' + tableStack);
+     } else {
+       console.log('No Seat Info');
+     }
+   });
+ }
 });
