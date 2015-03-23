a918b3caf84f1f1c8f9a7885483e72ec8c449b32
Dustin Morando <dustinmorando@me.com>
2015-03-23 17:47:37 -0400

    deleated a bunch of files

--- app/controllers/bankroll.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Controller.extend({
-  timeframe: ['All','Last 7 Days','Last 30 Days', 'Last 90 Days', 'Last 6 Month', 'Last 12 Month', 'Current Day', 'Current Week', 'Current Month', 'Custom Date Range'],
-  game: ['Texas Holdem', 'Omaha'],
-  structure: ['No Limit', 'Limit'],
-  limits: ['.25-.50', '1-2'],
-
-});
