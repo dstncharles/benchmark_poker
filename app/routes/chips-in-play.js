ea5632462c43cdab81743830711d6dc5cbabf8df
Dustin Morando <dustinmorando@me.com>
2015-03-23 15:35:02 -0400

    changed all the scss and html and fiddled with chartist

--- app/routes/chips-in-play.js
+++ app/routes/chips-in-play.js
@@ -6,7 +6,17 @@ export default Ember.Route.extend({
     return Ember.RSVP.hash({
       chips: this.findChipsInPlay(),
       comments: this.store.findAll('comment'),
-      newComment: this.store.createRecord('comment')
+      newComment: this.store.createRecord('comment'),
+
+      chartData: {
+        labels: ['Player1', 'Player2', 'Player3', 'Player4','Player5','Player6','Player7','Player8','Player9','Player10'],
+        series: [
+          [8, 6, 4, 5, 7, 9, 10, 2, 4, 1, 6],
+          [0, 2, 3, 2, 3],
+          [1, 2, 2.5, 3.5, 4]
+
+        ]
+      }
     });
   },
 
