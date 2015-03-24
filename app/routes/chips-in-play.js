e99947b1c6a80bc35fad1800e735fc142dfa1518
Dustin Morando <dustinmorando@me.com>
2015-03-24 12:11:49 -0400

    before lunch

--- app/routes/chips-in-play.js
+++ app/routes/chips-in-play.js
@@ -12,23 +12,35 @@ export default Ember.Route.extend({
   },
 
   actions: {
-    saveComment: function(){
-      console.log(this.modelFor('chips-in-play'));
+    saveComment: function() {
       this.modelFor('chips-in-play').newComment.save();
     }
   },
 
-  findChipsInPlay: function(){
-    return ajax('https://random-hands.herokuapp.com/hands/1').then(function(data) {
-      // data is an array of snapshots of the hand history after each hand is played
 
-      // gameData is the last game snapshot at the end of the game
-      //   and contains all the hands with all actions
-      var gameData = data.slice(-1).pop();
+//   deleteComment: function() {
+//     //if right what gos after deleteComment?
+//     this.modelFor('chips-in-play').deleteComment.destroy();
+//   }
+// },
+//
+// editComment: function() {
+//   this.modelFor('chips-in-play.edit').save();
+// }
 
-      // some of the hands are messed up. We'll filter those out by return only the hands that have seatsInfo
-      var cleanData = gameData.filterBy('seatsInfo');
-      return cleanData;
-    });
-  }
+
+
+findChipsInPlay: function() {
+  return ajax('https://random-hands.herokuapp.com/hands/1').then(function(data) {
+    // data is an array of snapshots of the hand history after each hand is played
+
+    // gameData is the last game snapshot at the end of the game
+    //   and contains all the hands with all actions
+    var gameData = data.slice(-1).pop();
+
+    // some of the hands are messed up. We'll filter those out by return only the hands that have seatsInfo
+    var cleanData = gameData.filterBy('seatsInfo');
+    return cleanData;
+  });
+}
 });
