6c9b5f6d52890808ba2039905b17d6fe97646d30
Dustin Morando <dustinmorando@me.com>
2015-03-25 21:44:34 -0400

    login page

--- app/controllers/dashboard.js
+++ app/controllers/dashboard.js
@@ -2,13 +2,23 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   stackSizes: function(){
-    return this.get('model.chips').mapBy('seatsInfo').map(function(handSeats){
+    // var data = this.get()
+    var data = this.get('model.chips').mapBy('seatsInfo').map(function(handSeats){
+
       //pluck(mapBy)
       return handSeats.mapBy('stackSize').reduce(function(sum, stackSize){
         // Number(stackSize) transforms stackSize from a string to a number
         return sum + Number(stackSize);
       }, 0);
     });
+
+    // return {
+    //   labels: Object.keys(data),
+    //   series: [Ember.$.map(data, function(count){
+    //     return count;
+    //   })]
+    // };
+
   }.property('model.chips.@each'),
 
   handCount: Ember.computed.alias('model.chips.length'),
